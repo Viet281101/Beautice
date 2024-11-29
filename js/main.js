d714ddcc3bbcd25a6637500836a4e9ac91455fe6
Viet281101 <nva281101@gmail.com>
2024-11-29 10:32:21 +0100

    add to top btn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import ProBox from './pro_box.js';
+import ToTopButton from './to_top.js';
 
 const proBoxHandler = new ProBox('.pro-box');
 proBoxHandler.initialize();
+
+const toTopButton = new ToTopButton('toTopBtn');
---
+++ js/to_top.js
@@ -0,0 +1,33 @@
+
+class ToTopButton {
+	constructor(buttonId) {
+		this.toTopBtn = document.getElementById(buttonId);
+		this.threshold = 0.8;
+		this.init();
+	}
+
+	toggleButtonVisibility = () => {
+		const scrollPosition = window.scrollY + window.innerHeight;
+		const documentHeight = document.documentElement.scrollHeight;
+
+		if (scrollPosition / documentHeight >= this.threshold) {
+			this.toTopBtn.classList.add('show');
+		} else {
+			this.toTopBtn.classList.remove('show');
+		}
+	};
+
+	scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	};
+
+	init = () => {
+		window.addEventListener('scroll', this.toggleButtonVisibility);
+		this.toTopBtn.addEventListener('click', this.scrollToTop);
+	};
+}
+
+export default ToTopButton;
